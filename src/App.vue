e10a48657c5d8479304f281678772a3122f769da
Abdur Rahim <arahimcse@gmail.com>
2023-08-14 01:26:42 +0600

    episode-01 code

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
+import UserInfo from './components/UserInfo.vue';
 </script>
 
 <template>
@@ -18,6 +19,7 @@ import HelloWorld from './components/HelloWorld.vue'
   </header>
 
   <RouterView />
+  <UserInfo/>
 </template>
 
 <style scoped>
