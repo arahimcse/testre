a7d50c560c78c37e69cdf5c19a55b66d638eda07
Abdur Rahim <arahimcse@gmail.com>
2023-08-17 20:43:44 +0600

    global component

---
+++ src/App1.vue
@@ -0,0 +1,13 @@
+<script  setup>
+import AAA from './components/comp1/AAA.vue';
+
+
+</script>
+
+<template>
+    <AAA/>
+</template>
+
+<style scoped>
+    
+</style>
--- src/main.js
+++ src/main.js
@@ -2,12 +2,15 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
+import App1 from './App1.vue'
 import router from './router'
 
 const app = createApp(App)
+const app1 = createApp(App1)
 
 app.use(router)
 
 app.mount('#app')
+app1.mount('#app1')
 
 
